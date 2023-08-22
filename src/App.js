4bfdf98da0833a7143be78772150f4288cb15686
raymondborel <rborel852@gmail.com>
2023-08-22 00:16:35 -0700

    Created Players component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Player from './components/Player';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> React Tac Toe</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Player/>
+    </>
   );
 }
 
---
+++ src/components/Player.js
@@ -0,0 +1,10 @@
+function Player() {
+    return (
+        <div>
+            <h2>Player </h2>
+            <h2>Wins: </h2>
+        </div>
+    )
+}
+
+export default Player
